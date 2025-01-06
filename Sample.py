822f03e06a9482488ac6ae73b261f89c6ff704de
fareshai <fareen.shaikh@capgemini.com>
2025-01-06 15:17:26 +0530

    Update and rename README.md to Sample.py

--- Sample.py
+++ Sample.py
@@ -1,7 +1,7 @@
 def reverse_sort(lst):
     return sorted(lst, reverse=True)
 
-# Example usage
+
 numbers = [5, 2, 9, 1, 5, 6]
 sorted_numbers = reverse_sort(numbers)
 print("Reverse sorted list:", sorted_numbers)
